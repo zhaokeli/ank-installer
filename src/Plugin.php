1b5f0aa6fbe06407d284dee6deb22006d77511d3
kelicom <735579768@qq.com>
2019-01-20 15:43:04 +0800

    f

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,6 +7,7 @@ use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Installer\PackageEvent;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
@@ -19,13 +20,20 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'post-update-cmd' => 'update',
+            'post-update-cmd' => 'cmdUpdate',
         );
     }
     public function install(PackageEvent $event)
     {
         // if ($event != null) {
         //     self::runScript($event, 'install');
+        // }
+    }
+    public function cmdUpdate(Event $event)
+    {
+        self::log('---------------------------------------------------plugin update');
+        // if ($event != null) {
+        //     self::runScript($event, 'update');
         // }
     }
     public function update(PackageEvent $event)
