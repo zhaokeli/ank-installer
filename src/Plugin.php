04db7f433c0c82c9fdb1ebe1109343eef88eb5bf
kelicom <735579768@qq.com>
2019-01-20 15:26:57 +0800

    f

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,10 +3,11 @@
 namespace mokuyu\ComposerInstallersExtender;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class Plugin implements PluginInterface
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
 
     public function activate(Composer $composer, IOInterface $io)
@@ -14,5 +15,29 @@ class Plugin implements PluginInterface
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
-
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-update-cmd' => 'update',
+        );
+    }
+    public static function install(PackageEvent $event)
+    {
+        // if ($event != null) {
+        //     self::runScript($event, 'install');
+        // }
+    }
+    public static function update(PackageEvent $event)
+    {
+        self::log('plugin update');
+        // if ($event != null) {
+        //     self::runScript($event, 'update');
+        // }
+    }
+    public static function uninstall(PackageEvent $event)
+    {
+        // if ($event != null) {
+        //     self::runScript($event, 'uninstall');
+        // }
+    }
 }
